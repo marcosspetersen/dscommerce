f6c47bf51b52bd75fde4561104b16b02683bb69c
marcosspetersen <marcos_petersen@hotmail.com>
2025-03-24 19:03:13 -0300

    Adicionado entity Role e seus relacionamentos com User.

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,10 +1,15 @@
 package com.devsuperior.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DscommerceApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -20,6 +18,12 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
@@ -82,6 +86,19 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public List<Order> getOrders() {
