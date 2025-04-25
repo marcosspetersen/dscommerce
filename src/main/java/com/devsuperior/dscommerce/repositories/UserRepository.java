465830593ac807401b3823b055edd4b497dd6ca8
marcosspetersen <marcos_petersen@hotmail.com>
2025-04-24 23:17:10 -0300

    Feito projection e query method para trazer user e roles.

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -1,9 +1,22 @@
 package com.devsuperior.dscommerce.repositories;
 
+import com.devsuperior.dscommerce.UserDetailsProjection;
 import com.devsuperior.dscommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u.email AS username, u.password, r.id AS roleId, r.authority " +
+            "FROM User u JOIN u.roles r " +
+            "WHERE u.email = :email")
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 }
--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,15 +1,35 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.UserDetailsProjection;
+import com.devsuperior.dscommerce.entities.Role;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if(result.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        result.forEach(u -> user.addRole(new Role(u.getRoleId(), u.getAuthority())));
+        return user;
     }
 }
